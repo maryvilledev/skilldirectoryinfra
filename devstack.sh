38ca080bec4cc2646a74156ce4fcc0f8aed9df75
Brian Hoehne <brian.hoehne@maryville.com>
2017-01-13 14:02:51 -0600

    added data and kill flags, stops failling when cassandra container doesn't exist

--- devstack.sh
+++ devstack.sh
@@ -5,6 +5,7 @@ export CASSANDRA_USERNAME=cassandra
 export CASSANDRA_PASSWORD=cassandra
 drop_data_flag=false
 export DEBUG_FLAG=true
+only_data=false
 
 ### Parse all command line flags
 for arg in "$@"
@@ -19,6 +20,13 @@ do
     docker-compose stop web backend
     docker-compose rm -f web backend
     exit 0
+  elif [[ $arg == "data"  ]]; then
+    echo "Only data"
+    only_data=true
+  elif [[ $arg == "kill"  ]]; then
+    docker-compose stop
+    docker-compose rm -f
+    exit 0
   elif [[ $arg == "pull" ]]; then
     docker-compose pull
     exit 0
@@ -29,9 +37,8 @@ do
   fi
 done
 
-### See if containers for Cassandra and/or Skilldirectory are cassandra_running
-cassandra_running=$(docker inspect -f {{.State.Running}} cassandra_container)
-skilldirectory_running=$(docker inspect -f {{.State.Running}} skilldirectory_container)
+### See if containers for Cassandra is running
+docker inspect -f {{.State.Running}} cassandra_container &&  cassandra_running="true"  || cassandra_running="false"
 
 ### If cassandra container is running and "--dropdata" flag was used, stop the container
 if $drop_data_flag; then
@@ -43,14 +50,12 @@ fi
 
 if ! $cassandra_running; then
   docker-compose up -d cassandra
-  sleep 25
+  sleep 40
 fi
-
-echo "API: $API"
 
 ### If "--dropdata" flag was used, drop the project's Cassandra keyspace within container
 if $drop_data_flag; then
-    echo 'Dropping and rebuilding "skill_directory_keyspace" keyspace'
+    echo "Dropping and rebuilding [skill_directory_keyspace] keyspace"
     docker exec -it cassandra_container bash usr/bin/cqlsh -u $CASSANDRA_USERNAME -p $CASSANDRA_PASSWORD -e "DROP KEYSPACE skill_directory_keyspace"
 fi
 
@@ -58,5 +63,10 @@ fi
 echo "Running skilldirectoryschema..."
 docker exec -it cassandra_container bash usr/bin/cqlsh -u $CASSANDRA_USERNAME -p $CASSANDRA_PASSWORD -f /data/skilldirectoryschema.cql
 echo "Schema update complete."
+
+if $only_data; then
+  echo "Exiting after starting cassandra"
+  exit 0
+fi
 
 docker-compose up -d --no-recreate
