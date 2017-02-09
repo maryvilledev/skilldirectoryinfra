5740d4e9f8a7c5bf84b2a9226104b110441d51cc
Brian Hoehne <brian.hoehne@maryville.com>
2017-02-09 11:31:57 -0600

    echo test status

--- devstack.sh
+++ devstack.sh
@@ -130,6 +130,7 @@ fi
 echo "$CASSANDRA_USERNAME $CASSANDRA_PASSWORD"
 
 docker-compose up -d --no-recreate backend web
+echo "Run Tests: $run_tests"
 if [[ $run_tests == true ]]; then
   docker-compose up -d backend-tester
 fi
